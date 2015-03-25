059ce707629df39bb4ffe578477b950109c4f811
RenzoNicolas <renzo_nicolas@outlook.com>
2015-03-24 23:49:19 -0400

    modified the chart application

--- js/C3ExGenerate.js
+++ js/C3ExGenerate.js
@@ -4,8 +4,16 @@ var chart = c3.generate({
       columns: [
         ['data1', 30, 200, 100, 400, 150, 250],
         ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
+      ],
+	  axes: {
+		  data2: 'y2'
+	  }
+    }, 
+	axis: {
+		y2: {
+			show: true
+		}
+	}
 });
 
 setTimeout(function () {
