34e8733fe54c4cf7195a8b3cefca2de48f2173a6
RenzoNicolas <renzo_nicolas@outlook.com>
2015-03-25 00:22:33 -0400

    added zoom functionality, subchart, resized, changed positon of legend, etc.

--- js/C3ExGenerate.js
+++ js/C3ExGenerate.js
@@ -1,19 +1,43 @@
 var chart = c3.generate({
+	  size: {
+        height: 350,
+        width: 480
+    },
     bindto: '#chart',
     data: {
       columns: [
         ['data1', 30, 200, 100, 400, 150, 250],
         ['data2', 50, 20, 10, 40, 15, 25]
       ],
-	  axes: {
-		  data2: 'y2'
-	  }
+	   types: {
+            data1: 'area'
+        },
+	   axes: {
+		    data2: 'y2'
+	    }
     }, 
+	grid: {
+        x: {
+            show: true
+        },
+        y: {
+            show: true
+        }
+    },
+	legend: {
+        position: 'right'
+    },
 	axis: {
 		y2: {
 			show: true
 		}
-	}
+	},
+	zoom: {
+        enabled: true
+    },
+	 subchart: {
+        show: true
+    }
 });
 
 setTimeout(function () {
