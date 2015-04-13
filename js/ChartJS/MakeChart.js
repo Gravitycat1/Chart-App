1d764760693a1aff405692e5a600cc524fd172a6
Gravitycat1 <gravitycat0@gmail.com>
2015-04-13 11:22:00 -0400

    trying to get chart to load data properly

--- js/ChartJS/MakeChart.js
+++ js/ChartJS/MakeChart.js
@@ -38,42 +38,41 @@
 	});
 }*/
 
-	
-
-
 function loadChart() {
- 
- 	console.log(priceData);
-			var chart = c3.generate({
-			  size: {
-				height: 350,
-				width: 480
+ var oldPriceData = null;
+ 	while ( oldPriceData = priceData) {
+ 		Runner.loadData(AppData, $('#stock')[0].value);
+ 	}
+ 		var chart = c3.generate({
+		  size: {
+			height: 350,
+			width: 480
+		},
+		bindto: '#chart',
+		data: {
+		  columns: [
+			['Price'].concat(priceData)
+		  ],
+		   types: {
+				Price: 'area'
 			},
-			bindto: '#chart',
-			data: {
-			  columns: [
-				['Price'].concat(priceData)
-			  ],
-			   types: {
-					Price: 'area'
-				},
-			}, 
-			point: {
-	        show: false
-			},
-			grid: {
-				x: {		
-			show: false
-				},
-				y: {
-					show: true
-				}
-			},
-			legend: {
+		}, 
+		point: {
+        show: false
+		},
+		grid: {
+			x: {
 				show: false
 			},
-			zoom: {
-				enabled: true
+			y: {
+				show: true
 			}
-		});
-	}
+		},
+		legend: {
+			show: false
+		},
+		zoom: {
+			enabled: true
+		}
+	});
+}
